7083a2e6bc2ca3b2837b1fc6171f254507872fc2
Steve <zzchenyufei@gmail.com>
2019-02-19 20:13:49 -0400

    Update main.py

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -60,13 +60,14 @@ def parseCourse(course_data):
             course['type'] = courseInfo[3].string
             course['credithours'] = courseInfo[4].string
             course['days'] = parseDate(courseInfo[6:11])
-
+            
+            '''
             stringCache = courseInfo[12]
             print(stringCache)
             stringCache.lstrip('\"')
             stringCache1 = stringCache.replace("\"","").replace("\"","").replace("\"","").replace("\"","")
-            print(stringCache1)
-            print(stringCache.find('<td class="dettb" nowrap="">&amp;nbsp&amp;'))
+            print(stringCache1)/'''
+            print(courseInfo[12].find('<td class="dettb" nowrap="">&amp;nbsp&amp;'))
             #Verify whether there are two time in two differnet time period
             if courseInfo[12].find("***")==0:
                 print("Oh shit!")
